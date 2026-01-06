9a0ccb1f6a764d6e6e2f1a07675db421b87c1db8
Cole_Yung <utsukushiotaku17@gmail.com>
2026-01-06 20:42:35 +0200

    Integrate UserProfile component into App
    Added UserProfile component with sample props.

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/UserProfile.jsx'
 import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
@@ -38,6 +39,12 @@ function App() {
             <MainContent />
             <Footer />
         </div>
+        <UserProfile
+    name="Alice"
+    age="25"
+    bio="Loves hiking and photography"
+/>
+
 
 export default App;
 
