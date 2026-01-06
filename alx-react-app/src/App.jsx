c63a06f638801087946639d3d6e60c3693c4979a
Cole_Yung <utsukushiotaku17@gmail.com>
2026-01-06 20:33:19 +0200

    Add Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,6 @@
+import Header from './components/Header.jsx'
+import MainContent from './components/MainContent.jsx'
+import Footer from './components/Footer.jsx'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -30,6 +33,14 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <WelcomeMessage />
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+
+export default App;
+
     </>
   )
 }
