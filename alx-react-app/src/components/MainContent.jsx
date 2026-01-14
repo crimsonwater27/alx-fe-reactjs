b21df2233a41dddc00cd7655bdfc43ae1a82181d
Cole_Yung <utsukushiotaku17@gmail.com>
2026-01-14 19:59:18 +0200

    Remove merge conflict markers from MainContent.jsx

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function MainContent() {
     return (
         <main>
@@ -8,14 +7,4 @@ function MainContent() {
 }
 
 export default MainContent;
-=======
-function MainContent() {
-    return (
-        <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
-        </main>
-    );
-}
 
-export default MainContent;
->>>>>>> eb507af62ed5280d16f1310fc83a17c68e5497c8
