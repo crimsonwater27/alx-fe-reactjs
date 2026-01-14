5327232a66f026845b94b6bc430533b2b5d1fb1c
Cole_Yung <utsukushiotaku17@gmail.com>
2026-01-14 19:58:03 +0200

    Fix Header component by resolving merge conflicts
    Removed merge conflict markers and cleaned up the Header component.

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function Header() {
     return (
         <header>
@@ -8,14 +7,4 @@ function Header() {
 }
 
 export default Header;
-=======
-function Header() {
-    return (
-        <header>
-            <h1>My Favorite Cities</h1>
-        </header>
-    );
-}
 
-export default Header;
->>>>>>> eb507af62ed5280d16f1310fc83a17c68e5497c8
