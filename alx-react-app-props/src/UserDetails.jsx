6336b0641261f50b16fbc706c6214635d2cc4409
Cole <utsukuskiotaku17@gmail.com>
2026-01-14 21:34:29 +0200

    edit name

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import UserContext from './UserContext';
+import UserContext from './src/UserContext';
 
 function UserDetails() {
   const userData = useContext(UserContext); // Access the user data from context
