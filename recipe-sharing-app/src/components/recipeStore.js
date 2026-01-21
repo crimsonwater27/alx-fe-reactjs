7e1a9e0a42bb021c2057e2dcfe3a4fcd2b043992
Cole <utsukuskiotaku17@gmail.com>
2026-01-21 18:33:10 +0200

    fix: correct syntax error in recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -8,6 +8,8 @@ export const useRecipeStore = create((set) => ({
       recipes: [...state.recipes, newRecipe],
     })),
 
+setRecipes: (recipes) => set({ recipes }),    
+
   deleteRecipe: (id) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== id),
