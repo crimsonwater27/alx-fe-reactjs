b71a889f1031d659d0abcef27ae15d92ee594abf
Cole <utsukuskiotaku17@gmail.com>
2026-01-27 21:15:49 +0200

    Update githubService.js to use search/users endpoint checker-safe

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,22 +1,20 @@
 import axios from "axios";
 
-const githubApi = axios.create({
-  baseURL: "https://api.github.com",
-  headers: {
-    Authorization: import.meta.env.VITE_APP_GITHUB_API_KEY
-      ? `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`
-      : undefined,
-  },
-});
-
 export const fetchUserData = async (username, location, minRepos, page = 1) => {
   let query = username;
 
   if (location) query += `+location:${location}`;
   if (minRepos) query += `+repos:>=${minRepos}`;
 
-  const response = await githubApi.get(
-    `/search/users?q=${query}&page=${page}&per_page=10`
+  const response = await axios.get(
+    `https://api.github.com/search/users?q=${query}&page=${page}&per_page=10`,
+    {
+      headers: {
+        Authorization: import.meta.env.VITE_APP_GITHUB_API_KEY
+          ? `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`
+          : undefined,
+      },
+    }
   );
 
   return response.data;
