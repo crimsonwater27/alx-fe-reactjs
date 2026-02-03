c9c98c27f7e3ca255ee27964757126d4101eb9bb
Cole <utsukuskiotaku17@gmail.com>
2026-02-03 19:04:09 +0200

    Refactor UserProfile component for improved readability and maintainability

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ function UserProfile() {
     <div className="
     bg-gray-100 
     sm:p-4 md:p-8 
-    max-w-sm md:max-w-sm 
+    max-w-xs md:max-w-sm 
     mx-auto my-20 
     rounded-lg 
     shadow-lg">
