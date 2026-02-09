13f5e551e9d05ee2ca311d35877a57fa035b0d46
Cole <utsukuskiotaku17@gmail.com>
2026-02-09 17:39:52 +0200

    install and configure Tailwind CSS with PostCSS and base directives

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-blue-500">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+  );
+}
